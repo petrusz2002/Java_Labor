89836eb5e27eebd7d1e8c36b24744e3ed0a014f0
matelazar07 <matelazar07@gmail.com>
2022-11-12 14:37:29 +0100

    close buttom

--- Java_Labor/src/main/java/com/mycompany/java_labor_projekt/MainFrame.java
+++ Java_Labor/src/main/java/com/mycompany/java_labor_projekt/MainFrame.java
@@ -7,6 +7,7 @@ package com.mycompany.java_labor_projekt;
 
 import com.opencsv.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -46,6 +47,7 @@ public class MainFrame extends javax.swing.JFrame {
         table = new javax.swing.JTable();
         bttn_import = new javax.swing.JButton();
         bttn_export = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java_Labor_Projekt");
@@ -109,6 +111,13 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -128,7 +137,8 @@ public class MainFrame extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(bttn_delete, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
-                            .addComponent(bttn_save, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(bttn_save, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton1))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -141,7 +151,9 @@ public class MainFrame extends javax.swing.JFrame {
                         .addGap(21, 21, 21)
                         .addComponent(bttn_save, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(bttn_delete, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(bttn_delete, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(36, 36, 36)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -249,6 +261,10 @@ public class MainFrame extends javax.swing.JFrame {
         //Keresés a táblában a bekért szöveg alapján
     }//GEN-LAST:event_bttn_findActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        close();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -283,6 +299,11 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
     }
+    public void close()
+    {
+        WindowEvent closeWindow = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+        Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(closeWindow);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bttn_delete;
@@ -290,6 +311,7 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JButton bttn_find;
     private javax.swing.JButton bttn_import;
     private javax.swing.JButton bttn_save;
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable table;
     private javax.swing.JTextField txt_input;
